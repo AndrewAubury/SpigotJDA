b8d400e56896143c91bab9c52eed0a0d9c68f162
Andrew Aubury <andrewaubury1@gmail.com>
2018-09-10 12:43:50 +0100

    Adding Plugin YML and calling events

--- src/main/java/me/andrew/spigotjda/events/JDAEventWrapper.java
+++ src/main/java/me/andrew/spigotjda/events/JDAEventWrapper.java
@@ -1,7 +1,9 @@
 package me.andrew.spigotjda.events;
 
+import me.andrew.spigotjda.events.custom.DiscordMessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
 
 /**
  * ------------------------------
@@ -15,7 +17,8 @@ public class JDAEventWrapper extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent e)
     {
-
+        DiscordMessageReceivedEvent event = new DiscordMessageReceivedEvent(e);
+        Bukkit.getServer().getPluginManager().callEvent(event);
     }
 
 
